d3fde25e8a0158d52390af16ce9b3ccdcb207bc1
AJ-Holzer <axel123.holzer@gmail.com>
2025-09-15 14:02:04 +0200

    Add function to validate Spotify playlist and track URLs with pattern matching

---
+++ src/utils/pot_url.py
@@ -0,0 +1,19 @@
+# ============================== #
+#  Copyright (c) AJ-Holzer       #
+#  SPDX-License-Identifier: MIT  #
+# ============================== #
+
+
+import re
+
+from api.spotify.types import PotUrl
+
+
+def validate_pot_url(pot_url: PotUrl) -> bool:
+    return (
+        re.fullmatch(
+            pattern=r"https://open.spotify.com/(playlist|track)/[A-Za-z0-9]{22}\?si=[A-Za-z0-9][16,32]",
+            string=pot_url,
+        )
+        is not None
+    )
